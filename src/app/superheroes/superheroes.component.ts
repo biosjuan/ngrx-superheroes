f202878c26916ffcdcd349a872f8ac0ff37460b7
biosjuan <hsoftwaredeveloperlee@gmail.com>
2023-09-18 14:32:27 -0700

    fix the call for superheropowers

--- src/app/superheroes/superheroes.component.ts
+++ src/app/superheroes/superheroes.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { SuperheroesEntityService } from './services/superheroes-entity.service';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map, of, switchMap } from 'rxjs';
 import { Superhero, SuperheroPower } from './models';
 import { SuperheroPowerEntityService } from './services/superhero-power-entity.service';
-import { QueryParams } from '@ngrx/data';
 
 @Component({
   selector: 'app-superheros',
@@ -19,14 +18,33 @@ export class SuperherosComponent implements OnInit {
     private superherosEntityService: SuperheroesEntityService,
     private superheroPowerEntityService: SuperheroPowerEntityService
   ) {}
+
   ngOnInit(): void {
     this.superheros$ = this.superherosEntityService.entities$;
   }
 
   getPowers(powersIds: number[]): void {
     const powerIdsQueryParam = powersIds.join(',');
-    this.superheroPowers$ = this.superheroPowerEntityService.getWithQuery({
-      powerIds: powerIdsQueryParam,
-    });
+
+    // Check if the data is already in the store
+    const powersInStore$ = this.superheroPowerEntityService.entities$.pipe(
+      // Filter the data based on the powerIds
+      map((powers) =>
+        powers.filter((power) => powersIds.includes(power.powerId))
+      )
+    );
+
+    this.superheroPowers$ = powersInStore$.pipe(
+      // If data is already in the store, return it; otherwise, make the HTTP request
+      switchMap((powersInStore) => {
+        if (powersInStore.length > 0) {
+          return of(powersInStore);
+        } else {
+          return this.superheroPowerEntityService.getWithQuery({
+            powerIds: powerIdsQueryParam,
+          });
+        }
+      })
+    );
   }
 }
